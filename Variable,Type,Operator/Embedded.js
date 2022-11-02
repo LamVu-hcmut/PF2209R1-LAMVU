4794a5512837bdcee5497e70cd2ae72468271c3a
LamVu-hcmut <vu.lam2404@hcmut.edu.vn>
2022-11-03 00:11:26 +0700

    Add embedded.js and a file embeddedJs.html

---
+++ Variable,Type,Operator/Embedded.js
@@ -0,0 +1,5 @@
+let message = `This is the beginning of my frontend journey! <br>`;
+function showMessage() {
+    document.getElementById('result').innerHTML = message;
+    message += `This is the beginning of my frontend journey! <br>`;
+}
